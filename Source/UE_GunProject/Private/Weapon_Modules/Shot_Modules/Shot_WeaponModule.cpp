81756eee4a99d7ed4612dc414473035c121901a3
Kiran George <gaurdian.kiran@gmail.com>
2025-08-26 20:16:40 +0200

    added editinnewline and introduced emitter modules

---
+++ Source/UE_GunProject/Private/Weapon_Modules/Shot_Modules/Shot_WeaponModule.cpp
@@ -0,0 +1,29 @@
+﻿#include "Weapon_Modules/Shot_Modules/Shot_WeaponModule.h"
+
+#include "Weapons/Weapon.h"
+
+UShot_WeaponModule::UShot_WeaponModule()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UShot_WeaponModule::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UShot_WeaponModule::AttachModule_Implementation(AActor* Actor)
+{
+	Super::AttachModule_Implementation(Actor);
+}
+
+void UShot_WeaponModule::IntegrateWithAttack_Implementation()
+{
+	DoTrace();
+}
+
+bool UShot_WeaponModule::ParentIsValid_Implementation()
+{
+	return Super::ParentIsValid_Implementation();
+}
+
